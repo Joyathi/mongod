27057076125b51807cdb5cf66efb8595d6e5c485
athin <athinjoy14chempananickal@gmail.com>
2024-05-14 09:48:36 +0530

    react

---
+++ React-landingpage/src/component/client/landingpage/Container.jsx
@@ -0,0 +1,10 @@
+function Container (){
+    return(
+        <>
+        <div className="container">
+         <h1>WECOME...</h1>
+        </div>
+        </>
+    )
+}
+export default Container;
---
+++ React-landingpage/src/component/client/landingpage/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Navigation(){
+    return(
+        <>
+        <nav>
+
+            <div className="navmain">
+                <ul className="items">
+                    <li>
+                        <button>About</button>
+                    </li>
+                    <li>
+                        <button>Login</button>
+                    </li>
+                    <li>
+                        <button>Sign In</button>
+                    </li>
+                    <li>
+                        <button>Support</button>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+        </>
+    )
+}
+export default Navigation;
