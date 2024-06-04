f816c28e74b83f907102974b17ba0ed3db8fc7bd
athin <athinjoy14chempananickal@gmail.com>
2024-06-04 07:10:15 +0530

    react

--- Project-Work/client/UMS/src/Components/LogInPage/LogIn.jsx
+++ Project-Work/client/UMS/src/Components/LogInPage/LogIn.jsx
@@ -1,33 +1,31 @@
 import { Link } from "react-router-dom";
-function LogIn() {
-    return (
-        <>
-            <link rel="stylesheet" href="../../../LogIn.css" />
-            <form action="/submit" method="POST">
-               
-                <div id="email-group">
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" onKeyUp="validateEmail()" />
-                    <div id="email-error" />
-                </div>
-                <div id="password-group">
-                    <label htmlFor="password">Password: </label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        onkeyup="validatePassword()"
-                    />
-                    <div id="password-error" />
-                </div>
-                <div className="btn">
-                    <button type="button" onClick="submitForm()">LogIn</button>
-                </div>
-            </form>
-            
-        </>
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
+import "./Login.css"
+import React, {useState} from "react"
+import axios from "axios";
 
-    )
-}
+import UserNAv from "../UserPage/UserNav";
 
-export default LogIn;
+const Login = () => {
+    const submitForm = async (email,password) =>{
+        try{
+            const response = await axios.post("http://localhost:5173")
+            email:email,
+            password: password
+    
+    }}
+         console.log ("response :",response)
+         if(response.data && response.data.success){
+            console.log("reponse.data.data:",response.data.data)
+            localStorage.setItem('jwtToken', response.data.data)
+            console.log('Token saved to localStorege');
+         }else{
+            console.error('Token not found in response');
+            alert("Token not found in response")
+         }catch (error){
+            console.error('Error fetching the token:',error);
+            alert("Something went Wrong")
+
+         
+    }
+};
