6da0d257e8c372ce4257f60d7eb1ec5297cfeb23
athin <athinjoy14chempananickal@gmail.com>
2024-06-04 10:10:11 +0530

    athin

--- Project-Work/server/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-
-router.get('/users', userController.getUsers);
-
-module.exports = router; 
